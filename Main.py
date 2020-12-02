e71440689c01f00d32e19fcbf009231bd41a1b83
Josh Ye <joshua.ye@duke.edu>
2020-12-02 14:57:04 -0500

    data now imports correctly

--- Main.py
+++ Main.py
@@ -12,17 +12,18 @@ def getCovidData(date, region):
 
     if (region == 'us'):
         url = "https://api.covidtracking.com/v1/us/" + date + ".json"
+        if (date == 'current'):
+            data = json.loads(requests.get(url).text)[0]
+        else:
+            data = json.loads(requests.get(url).text)
     else:
         url = "https://api.covidtracking.com/v1/states/" + region + "/" + date + ".json"
-
-    if (date == 'current'):
-        data = json.loads(requests.get(url).text)[0]
-    else:
         data = json.loads(requests.get(url).text)
 
 
-    for key in data:
-        print(key + ' : ' + str(data[key]))
+    #for key in data:
+        #print(key + ' : ' + str(data[key]))
+    return data
 
 def lowerStringVar(var):
     """Function to convert the text in a StringVar to lower case"""
--- MainGui.py
+++ MainGui.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import Overrides
 import Main
 from datetime import date
+import tktable
 
 #Create class mainGui as instance of tk.Tk
 class MainGui(tk.Tk):
@@ -10,29 +11,40 @@ class MainGui(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        #set instance variable for data
+        self.dataDict = None
+
         #set title
         self.title("Covid Data")
 
         #set dimensions
-        self.geometry('500x250')
+        self.geometry('1000x500')
 
         # label text for title
         ttk.Label(self, text="Covid Data Grabber",
                   background='green', foreground="white",
                   font=("Times New Roman", 15)).grid(row=0, column=1)
 
+        #Create frame for drop-down menus and button
+        inputFrame = ttk.Frame(self, relief = tk.SUNKEN, borderwidth = 2)
+        outputFrame = ttk.Frame(self, relief = tk.SUNKEN, borderwidth = 2)
+
+
+        inputFrame.grid(row=16, column=1, rowspan = 15, columnspan = 1, padx=10, pady=10, sticky = tk.W)
+        outputFrame.grid(row = 1, column = 1, rowspan = 15, columnspan = 1, padx = 10, pady = 10, sticky = tk.W)
+
         # Create Region and Date Labels
-        ttk.Label(self, text="Select the Region :",
-                  font=("Times New Roman", 10)).grid(column=0,
+        ttk.Label(inputFrame, text="Select the Region :",
+                  font=("Times New Roman", 10), width=20).grid(column=0,
                                                      row=5, padx=10, pady=25, sticky = tk.W)
 
-        ttk.Label(self, text="Select the date :",
-                  font=("Times New Roman", 10)).grid(column=0,
-                                                     row=10, padx=10, pady=25, sticky = tk.W)
+        ttk.Label(inputFrame, text="Select the date :",
+                  font=("Times New Roman", 10), width=20).grid(column=0,
+                                                     row=8, padx=10, pady=25, sticky = tk.W)
 
         #make region dropdown menu
         n = tk.StringVar()
-        region = ttk.Combobox(self, width=15, textvariable=n)
+        region = ttk.Combobox(inputFrame, width=20, textvariable=n)
 
         # Adding combobox drop down list
         region['values'] = ("US", "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
@@ -47,13 +59,12 @@ class MainGui(tk.Tk):
 
 
         #create cal
-        #cal = tkcal.DateEntry(self, date_pattern='ymmdd', width=15W, bg="darkblue", fg="white", year=2020)
 
         d = tk.StringVar()
 
-        cal = Overrides.CustomDateEntry(self, textvariable = d, date_pattern='yyyymmdd', width=15, bg="darkblue", fg="white", year=2020)
+        cal = Overrides.CustomDateEntry(inputFrame, textvariable = d, date_pattern='yyyymmdd', width=20, bg="darkblue", fg="white", year=2020)
 
-        cal.grid(column=1, row=10, sticky = tk.W)
+        cal.grid(column=1, row=8, sticky = tk.W)
 
         #create submit button
 
@@ -66,16 +77,49 @@ class MainGui(tk.Tk):
             else:
                 dateString = d.get()
 
-            Main.getCovidData(dateString,regionString)
+            #set instance variable dataDict to the grabbed covid data
+            self.dataDict = Main.getCovidData(dateString,regionString)
 
+            #turn keys into a list
+            labelList = []
+            for key in self.dataDict.keys():
+                labelList.append(key)
 
-        btn = ttk.Button(self, text='Get Data',
+            #Add labels in data table
+            labels = labelList
+
+            for x in range(len(labels)):
+                var = tk.StringVar()
+                label = tk.Label(outputFrame, textvariable=var, width=20, anchor = tk.W)
+                var.set(labels[x])
+                label.grid(column=1, padx=10, row=2 * x + 1, sticky=tk.W, columnspan=1)
+
+                out = tk.StringVar()
+                output = tk.Entry(outputFrame, textvariable=out, width=22)
+                out.set(self.dataDict[labels[x]])
+                output.grid(column=2, padx=10, row=2 * x + 1, sticky=tk.W, columnspan=1)
+
+        btn = ttk.Button(inputFrame, text='Get Data',
                      command= getData)
 
-        # Set the position of button on the top of window.
+        # Set the position of button on the bottom of window.
         btn.grid(column=1, row=15, sticky = tk.W)
 
-        #Create Entries to store JSON Data
+        #create labels
+
+        v = tk.StringVar()
+        categoryLabel = tk.Label(outputFrame, textvariable=v,font=("Times New Roman", 15), width=12, anchor = tk.W)
+        v.set("Category")
+        categoryLabel.grid(column=1, row=0, padx=5, pady=15, sticky=tk.W, columnspan=1)
+
+        o = tk.StringVar()
+        output = tk.Label(outputFrame, textvariable=o,font=("Times New Roman", 15), width=11, anchor = tk.W)
+        o.set("Value")
+        output.grid(column=2, row=0, padx=5, pady=15, sticky=tk.W, columnspan=1)
+
+
+        #Create Table to store JSON Data
+
 
 
 if __name__ == "__main__":
